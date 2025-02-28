cd9c71e55520bfcd5e0d3727c826c74613e006aa
121yguy <156339882+121yguy@users.noreply.github.com>
2025-02-28 16:44:02 +0800

    Add files via upload

---
+++ back/src/test/java/com/myzhihu/MyZhihuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myzhihu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyZhihuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
