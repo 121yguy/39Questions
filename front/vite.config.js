786520ca7fe210ea3e05f1a6310737889de02b5a
121yguy <156339882+121yguy@users.noreply.github.com>
2025-02-28 17:00:17 +0800

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:${your-server-port}',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/,"")
       }
