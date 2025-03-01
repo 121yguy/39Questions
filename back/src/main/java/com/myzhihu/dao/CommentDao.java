e53627ba57aad75c67f9ca36067cb6a54974ae9e
121yguy <156339882+121yguy@users.noreply.github.com>
2025-03-01 18:28:55 +0800

    Update CommentDao.java

--- back/src/main/java/com/myzhihu/dao/CommentDao.java
+++ back/src/main/java/com/myzhihu/dao/CommentDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Mapper
 public interface CommentDao {
 
-    @Select("SELECT comments.id, content, author_id AS authorId, time, icon, nickName FROM comments JOIN my_zhihu_db.user_info u on u.userId = comments.author_id WHERE comments.id < #{startId} ORDER BY id DESC LIMIT 12")
+    @Select("SELECT comments.id, content, author_id AS authorId, time, icon, nickName FROM comments JOIN user_info u on u.userId = comments.author_id WHERE comments.id < #{startId} ORDER BY id DESC LIMIT 12")
     List<CommentWithUserInfo> getCommentsByStartId(long startId);
 
     @Select("SELECT id FROM comments WHERE author_id = #{uid} AND id = #{id}")
